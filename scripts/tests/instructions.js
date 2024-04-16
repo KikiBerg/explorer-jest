9b0ec8f30b32d097da721d760caa8430e2d826b3
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-04-16 15:27:04 +0000

    add content

---
+++ scripts/tests/instructions.js
@@ -0,0 +1,6 @@
+// npm init
+//test command: jest
+// keep all the other defaults
+// npm install jest --save-dev
+// try npm test
+// Install jest-environment-jsdom: npm install --save-dev jest-environment-jsdom
---
+++ scripts/tests/jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom',
+    // Other Jest configurations (if any)
+  };
